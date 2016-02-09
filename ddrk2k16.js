d2f59e8f2765e28238f4f73a6b8be9db9686d0d0
devidasm <devidasmlry@gmail.com>
2016-02-09 22:32:16 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -1,62 +1,35 @@
 jQuery(document).ready(function() {
-	"use strict";
+    "use strict";
 
-	var $ = jQuery,
-	html = $('html');
+    var $ = jQuery,
+        html = $('html');
 
-	// IE<8 Warning
-	if (html.hasClass("ie6") || html.hasClass("ie7")) {
-		$("body").empty().html('UPDATE YOUR BROWSER');
-	}
+    // IE<8 Warning
+    if (html.hasClass("ie6") || html.hasClass("ie7")) {
+        $("body").empty().html('UPDATE YOUR BROWSER');
+    }
 
 
 
-	// Listing Item
-	( function() {
-		var store_listing_item = $('.coupon-container');
-		store_listing_item.each( function(){
-			// Open Modal
-		//	var coupon_modal = $(this).find('.coupon-modal');
-		//	var coupon_id = coupon_modal.attr('id');
-			$(this).find('button button2').click( function () {
-				var aff_url = $(this).attr('data-aff-url');
-				var current_url = $(location).attr('href');
-				window.open(aff_url, '_self');
-				window.open('#'+ 'coupon','_blank');
-			//	$(coupon_modal).modal('show');
-				return false;
-			} );
-			var coupon_hash = $(location).attr('hash');
-			if ( coupon_hash == '#'+coupon_id ) {
-				$(coupon_modal).modal('show');
-			}
+    // Listing Item
+    ( function() {
+        var store_listing_item = $('.coupon-container');
+        store_listing_item.each( function(){
+            // Open Modal
+            //	var coupon_modal = $(this).find('.coupon-modal');
+            //	var coupon_id = coupon_modal.attr('id');
+            $(this).find('button button2').click( function () {
+                var aff_url = $(this).attr('data-aff-url');
+                var current_url = $(location).attr('href');
+                window.open(aff_url, '_self');
+                window.open('#'+ 'coupon','_blank');
+                //	$(coupon_modal).modal('show');
+                return false;
+            } );
+           
 
-			// Reveal box
-			var reveal_link = $(this).find('.coupon-footer li a');
-			var reveal_content = $(this).find('.reveal-content');
-			reveal_link.each( function() {
-				$(this).click( function(){
-
-					reveal_link.removeClass('active');
-					$(this).addClass('active');
-					//$(this).toggleClass('active');
-
-					var reveal_link_data = $(this).attr('data-reveal');
-					reveal_content.each( function() {
-						if( $(this).hasClass(reveal_link_data) ) {
-							reveal_content.removeClass('active');
-							$(this).addClass('active');
-							
-							$(this).find('.close').on( 'click', function() {
-								$(this).parent().removeClass('active');
-								reveal_link.removeClass('active');
-							});
-						}
-					});
-					return false;
-				});
-			} );
-		});
-	} )();
+           
+        });
+    } )();
 
 });
