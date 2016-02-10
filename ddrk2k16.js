fba5b49822980aa923b086150a6214d921d0d9ef
devidasm <devidasmlry@gmail.com>
2016-02-10 21:06:53 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -20,8 +20,8 @@ jQuery(document).ready(function() {
             //	var coupon_id = coupon_modal.attr('id');
             $(this).find('button').click( function () {
                 var aff_url = $(this).attr('data-aff-url');
-                aff_url = 'http://www.google.com';
-
+                //aff_url = 'http://www.google.com';
+console.log('URLLLLL'+ 'aff_url');
                 //alert(aff_url);
                 var current_url = $(location).attr('href');
                 window.open(aff_url, '_self');
