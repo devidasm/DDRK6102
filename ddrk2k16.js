714004de1e841617f519036a77b4f07402614f03
devidasm <devidasmlry@gmail.com>
2016-02-10 21:00:13 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function() {
             //	var coupon_id = coupon_modal.attr('id');
             $(this).find('button').click( function () {
                 var aff_url = $(this).attr('data-aff-url');
-                aff_url = "http://www.google.com";
+                aff_url = 'http://www.google.com';
 
                 //alert(aff_url);
                 var current_url = $(location).attr('href');
