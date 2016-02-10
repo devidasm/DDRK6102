1fc6682cac70ec46e2f397a393bb1bfaee9652ed
devidasm <devidasmlry@gmail.com>
2016-02-10 20:50:37 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function() {
             //	var coupon_id = coupon_modal.attr('id');
             $(this).find('button').click( function () {
                 var aff_url = $(this).attr('data-aff-url');
-                alert(aff_url);
+                //alert(aff_url);
                 var current_url = $(location).attr('href');
                 window.open(aff_url, '_self');
                 window.open('#'+ 'coupon','_blank');
