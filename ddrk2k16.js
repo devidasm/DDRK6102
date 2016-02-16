2d4c598e995da5b528881b73aa41e70586daa3d1
devidasm <devidasmlry@gmail.com>
2016-02-16 07:48:08 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -20,13 +20,19 @@ jQuery(document).ready(function() {
             //	var coupon_id = coupon_modal.attr('id');
             $(this).find('button').click( function () {
                 var aff_url = $(this).attr('offer-url');
-                var desc = $(this).attr('coupon h3');
+                var desc = $(this).attr('coupon-desc');
                 var cpc = $(this).attr('cp_code');
-
-
-                $(".offer-desc").text(desc);  
-                 $(".code-text").text(cpc);
-                        $('#fadeandscale').popup('show');
+            if(cpc!=nil)
+            {
+              $(".offer-desc").text(desc);  
+              $(".code-text").text(cpc);
+            }
+             else     
+             {
+              $(".offer-desc").text('No Coupon Code Required');  
+              $(".code-text").text('DEAL ACTIVATED');
+             }
+             $('#fadeandscale').popup('show');
                  window.open(aff_url, '_blank');
                 window.open('#'+ 'coupon','_self');
                
