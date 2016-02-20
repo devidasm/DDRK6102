f7119ba0bab35bf9abb2f93dd1bffb871044407f
devidasm <devidasmlry@gmail.com>
2016-02-20 17:00:43 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
                 var desc = $(this).attr('coupon-desc');
                 var cpc = $(this).attr('cp_code');
                 
-                 
+                 alert(desc);
             if(cpc!=undefined)
             {
               $(".offer-desc").text(desc);  
