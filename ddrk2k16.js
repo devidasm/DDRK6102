caf17151d746d195bb40772809adc5894a6ac03e
devidasm <devidasmlry@gmail.com>
2016-02-20 16:47:04 +0530

    Update ddrk2k16.js

--- ddrk2k16.js
+++ ddrk2k16.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function() {
                 var cpc = $(this).attr('cp_code');
                 
                  
-            if(cpc!='undefined')
+            if(cpc!=undefined)
             {
               $(".offer-desc").text(desc);  
               $(".code-text").text(cpc);
